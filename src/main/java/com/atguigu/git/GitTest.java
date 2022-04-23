cbbcc935bc7b8e85a64fa458faced7230bb5493b
zh <zh@xxyy.com>
2022-04-23 21:53:48 +0800

    git commit to 122

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,15 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!1");
-        System.out.println("Hello Git!2");
-        System.out.println("Hello Git3");
-        System.out.println("Hello Git4");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push test ");       
-        System.out.println("pull test ");
-        System.out.println("gitee test ");
 
-         System.out.println("gitee test22 ");
     }
 }
