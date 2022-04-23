8aeda2d9a7ef45d450af45dc13b7174ce90bfe7d
zh <zh@xxyy.com>
2022-04-23 16:06:26 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,6 +2,7 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git!1");
+        System.out.println("Hello Git!2");
     }
 }
